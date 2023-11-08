d2464a4de21b8efd805828aa12aad80b52d226aa
omarx <omarpeart100@gmail.com>
2023-11-07 19:51:55 -0500

    Adjusted page to match the first image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,15 @@
-function loadAvengers(){
+document.addEventListener('DOMContentLoaded',()=>{
+    const listView=document.getElementById('main-section');
+
     fetch('/superheroes.php')
         .then(response=>response.text())
-        .then(data=>alert(data))
+        .then(data=>
+            {
+                listView.innerHTML=data;
+            }
+        )
         .catch(error=>{
             console.error("No response from server: ",error);
         })
-}
+
+})
